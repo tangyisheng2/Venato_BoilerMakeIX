72a2aa4708d1afebe899e62a94bd71de3a9b8dac
Eason Tang <tangyisheng2@sina.com>
2022-01-22 13:43:06 -0800

    Add "Get Meal" API
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/meal/meal.py
+++ apis/meal/meal.py
@@ -3,16 +3,40 @@
 # @FileName  :meal.py
 # @Time      :1/21/22
 # @Author    :Eason Tang
+from flask import request
 from flask_restful import Resource
+import db
 
 
 class Meal(Resource):
-    @staticmethod
-    def get():
+    def __init__(self):
+        self.db_session = db.DB()
+
+    def get(self):
         """
         This function gets all the meals that the user have
         :return: Meal:id
         """
+        self.db_session.init_connection()
+        try:
+            json_data = request.get_json()
+            if "user_id" in json_data:
+                user_id = json_data['user_id']
+                # Check if user id is existed
+                sql = "SELECT id FROM production.user WHERE id = %d" % user_id
+                status, err, ret = self.db_session.query(sql)
+                if not ret:
+                    return {"status": -1, "msg": "User id does not exist"}
+                # Get Meals from user id
+                # todo Take care of the limit here
+                sql = "SELECT * FROM production.meal WHERE user_id = %d LIMIT 20" % user_id
+                status, err, ret = self.db_session.query(sql)
+                if status == 0:
+                    return {"status": 0, "msg": ret}
+                else:
+                    return {"status": status, "msg": "Something went wrong"}
+        finally:
+            self.db_session.close()
         return "This function gets all the meals that the user have"
 
     @staticmethod

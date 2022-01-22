f57a158c67aba9fa5b88d4b62ba4fa8a68a81aa5
Eason Tang <tangyisheng2@sina.com>
2022-01-22 10:17:09 -0800

    Add register function
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/user/user.py
+++ apis/user/user.py
@@ -5,24 +5,58 @@
 # @Author    :Eason Tang
 from flask import request
 from flask_restful import Resource, reqparse
+import db
 
 
 class LogIn(Resource):
-    def post(self):
+    def __init__(self):
+        self.db_session = db.DB()
+
+    def post(self, username):
         """
         This function takes username and password and create a user
         :return: User info
         """
         json_data = request.get_json()  # Where it takes json data
-        return json_data
+        if json_data['username'] and json_data['password']:
+            sql = f'SELECT t.*  FROM test.user t  ' \
+                  f'WHERE username = "{str(json_data["username"])}" ' \
+                  f'AND password = "{str(json_data["password"])}" LIMIT  1'
+            status, errmsg, ret = self.db_session.query(sql)
+            if status == 0:  # Login Success
+                return {"status": 0, "userid": ret[0]['id'], "username": ret[0]['username']}
+            else:
+                return {"status": status, "errmsg": errmsg}
+        else:
+            return {"status": -1, "errmsg": "Login Failed"}
 
 
 class SignUp(Resource):
+    def __init__(self):
+        self.db_session = db.DB()
+
     def post(self):
         """
         This function takes username and password, verify it and login and return the userinfo
         :return:
         """
         json_data = request.get_json()
-        return json_data
-        return "This function takes username and password, verify it and login and return the userinfo"
+        if json_data['username'] and json_data['password']:
+            # Register User
+            sql = f'INSERT INTO test.user (username,password) ' \
+                  f'VALUES ("{json_data["username"]}","{json_data["password"]}")'
+            status, errmsg, ret = self.db_session.query(sql)
+            if status == 0:  # Login Success
+                # Fetch User
+                sql = f'SELECT t.*  FROM test.user t  ' \
+                      f'WHERE username = "{str(json_data["username"])}" ' \
+                      f'AND password = "{str(json_data["password"])}" LIMIT  1'
+                status, errmsg, ret = self.db_session.query(sql)
+                if status == 0:
+                    return {"status": 0, "userid": ret[0]['id'], "username": ret[0]['username']}
+                else:
+                    return {"status": status, "errmsg": errmsg}
+            else:
+                return {"status": -1, "errmsg": "Some error occur, registration failed, the username might already taken"}
+        else:
+            return {"status": -2, "errmsg": "Username and password no valid"}

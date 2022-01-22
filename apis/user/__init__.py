4574ad8da26c7eca7db52c9a16307a335eda70e9
Eason Tang <tangyisheng2@sina.com>
2022-01-22 13:24:14 -0800

    Add statics for analyze system
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/user/__init__.py
+++ apis/user/__init__.py
@@ -8,9 +8,11 @@ from flask_restful import Api
 
 from .user import SignUp, LogIn
 from .comsumption import Comsumption
+from .statics import Statics
 
 user_bp = Blueprint('user', __name__)
 api = Api(user_bp)
 api.add_resource(SignUp, '/user/signup')
 api.add_resource(LogIn, '/user/login')
 api.add_resource(Comsumption, "/user/consumption")
+api.add_resource(Statics, "/user/statics")
---
+++ apis/user/statics.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :statics.py
+# @Time      :1/22/22
+# @Author    :Eason Tang
+from flask import request
+from flask_restful import Resource
+import db
+
+
+class Statics(Resource):
+    def __init__(self):
+        self.db_session = db.DB()
+
+    def get(self):
+        """
+        This function returns the summary of the nutrition data
+        :return:
+        """
+        self.db_session.init_connection()
+        try:
+            # json_data = request.get_json()
+            # if len(json_data) == 1 and "user_id" in json_data:
+            #     user_id = json_data['user_id']
+            # # Check if user id is existed
+            # sql = "SELECT id FROM production.user WHERE id = %d" % user_id
+            # status, err, ret = self.db_session.query(sql)
+            # if not ret:
+            #     return {"status": -1, "msg": "User id does not exist"}
+            # Get the statics
+            sql = "SELECT production.grocery.id, " \
+                  "SUM(production.grocery.amount_g) AS 'amount_g', production.nutrition.category as category " \
+                  "FROM production.grocery " \
+                  "JOIN production.nutrition " \
+                  "ON grocery.nutrition_id = nutrition.id " \
+                  "GROUP BY category"
+            status, err, ret = self.db_session.query(sql)
+            return {"status": 0, "msg": ret}
+
+
+
+        finally:
+            self.db_session.close()

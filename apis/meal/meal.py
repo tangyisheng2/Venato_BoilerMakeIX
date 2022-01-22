6ead19d55afa1d4adeb34bd5defc815d918499fe
Eason Tang <tangyisheng2@sina.com>
2022-01-22 14:11:08 -0800

    Add "Create Meal" API
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/meal/meal.py
+++ apis/meal/meal.py
@@ -39,10 +39,41 @@ class Meal(Resource):
             self.db_session.close()
         return "This function gets all the meals that the user have"
 
-    @staticmethod
-    def post():
+    def post(self):
         """
         This function adds a meal to the user database
+        CreateMeal(User.id, name, Grocery.id[], grocery_weights[]): Meal
+        # will delete corresponding amounts of grams from Groceries. If the amount of grams in “grocery_weights” is greater than or equal to Grocery.amt_g then change the “grocery_weight” to match it and delete the grocery from the user
+        # and create meal
         :return: Meal[]
         """
-        return "This function adds a meal to the user database"
+        self.db_session.init_connection()
+        try:
+            json_data = request.get_json()
+            # todo refine api
+            if len(json_data) >= 3 and "user_id" in json_data and "name" in json_data and "grocery" in json_data:
+                # todo parse ingredient data to subtract from grocery
+                # Insert Data
+                user_id = json_data['user_id']
+                name = json_data['name']
+                grocery = json_data['grocery']
+                # sql = 'INSERT INTO production.meal (user_id, name, image_url) VALUES ("%d", "%s", "%s")' \
+                #       % (user_id, name, grocery)
+                sql = 'INSERT INTO production.meal (user_id, name, image_url) VALUES ("%d", "%s", "%s")' \
+                      % (user_id, name, None)
+                status, err, ret = self.db_session.query(sql)
+                # Get the last insert ID
+                sql = "SELECT last_insert_id()"
+                status, err, ret = self.db_session.query(sql)
+                last_id = ret[0]['last_insert_id()']
+                # Fetch the inserted data
+                sql = 'SELECT * FROM production.meal WHERE id = %d LIMIT 1' % last_id
+                status, err, ret = self.db_session.query(sql)
+                if status == 0:
+                    return {"status": status, "msg": ret}
+                else:
+                    return {"status": status, "msg": "Some thing went wrong"}
+            else:
+                return {"status": -1, "msg": "invalid input"}
+        finally:
+            self.db_session.close()

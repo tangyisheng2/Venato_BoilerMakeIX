789654e620b8f609522d4b48168e52d6937f0b12
Eason Tang <tangyisheng2@sina.com>
2022-01-22 11:33:21 -0800

    Update user module to support continuous connection for db
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/user/user.py
+++ apis/user/user.py
@@ -17,18 +17,22 @@ class LogIn(Resource):
         This function takes username and password and create a user
         :return: User info
         """
-        json_data = request.get_json()  # Where it takes json data
-        if json_data['username'] and json_data['password']:
-            sql = f'SELECT t.*  FROM test.user t  ' \
-                  f'WHERE username = "{str(json_data["username"])}" ' \
-                  f'AND password = "{str(json_data["password"])}" LIMIT  1'
-            status, errmsg, ret = self.db_session.query(sql)
-            if status == 0:  # Login Success
-                return {"status": 0, "msg": {"userid": ret[0]['id'], "username": ret[0]['username']}}
+        self.db_session.init_connection()
+        try:
+            json_data = request.get_json()  # Where it takes json data
+            if json_data['username'] and json_data['password']:
+                sql = f'SELECT t.*  FROM test.user t  ' \
+                      f'WHERE username = "{str(json_data["username"])}" ' \
+                      f'AND password = "{str(json_data["password"])}" LIMIT  1'
+                status, errmsg, ret = self.db_session.query(sql)
+                if status == 0:  # Login Success
+                    return {"status": 0, "msg": {"userid": ret[0]['id'], "username": ret[0]['username']}}
+                else:
+                    return {"status": status, "msg": errmsg}
             else:
-                return {"status": status, "msg": errmsg}
-        else:
-            return {"status": -1, "msg": "Login Failed"}
+                return {"status": -1, "msg": "Login Failed"}
+        finally:
+            self.db_session.close()
 
 
 class SignUp(Resource):
@@ -40,23 +44,27 @@ class SignUp(Resource):
         This function takes username and password, verify it and login and return the userinfo
         :return:
         """
-        json_data = request.get_json()
-        if json_data['username'] and json_data['password']:
-            # Register User
-            sql = f'INSERT INTO test.user (username,password) ' \
-                  f'VALUES ("{json_data["username"]}","{json_data["password"]}")'
-            status, errmsg, ret = self.db_session.query(sql)
-            if status == 0:  # Login Success
-                # Fetch User
-                sql = f'SELECT t.*  FROM test.user t  ' \
-                      f'WHERE username = "{str(json_data["username"])}" ' \
-                      f'AND password = "{str(json_data["password"])}" LIMIT  1'
+        self.db_session.init_connection()
+        try:
+            json_data = request.get_json()
+            if json_data['username'] and json_data['password']:
+                # Register User
+                sql = f'INSERT INTO test.user (username,password) ' \
+                      f'VALUES ("{json_data["username"]}","{json_data["password"]}")'
                 status, errmsg, ret = self.db_session.query(sql)
-                if status == 0:
-                    return {"status": 0, "msg": {"userid": ret[0]['id'], "username": ret[0]['username']}}
+                if status == 0:  # Login Success
+                    # Fetch User
+                    sql = f'SELECT t.*  FROM test.user t  ' \
+                          f'WHERE username = "{str(json_data["username"])}" ' \
+                          f'AND password = "{str(json_data["password"])}" LIMIT  1'
+                    status, errmsg, ret = self.db_session.query(sql)
+                    if status == 0:
+                        return {"status": 0, "msg": {"userid": ret[0]['id'], "username": ret[0]['username']}}
+                    else:
+                        return {"status": status, "msg": errmsg}
                 else:
-                    return {"status": status, "msg": errmsg}
+                    return {"status": -1, "msg": "Some error occur, registration failed, the username might already taken"}
             else:
-                return {"status": -1, "msg": "Some error occur, registration failed, the username might already taken"}
-        else:
-            return {"status": -2, "msg": "Username and password no valid"}
+                return {"status": -2, "msg": "Username and password no valid"}
+        finally:
+            self.db_session.close()

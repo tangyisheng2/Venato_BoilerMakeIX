3670411d9349ba340d6389eca50063b8498a7964
tangyisheng2 <tangyisheng2@sina.com>
2022-01-22 01:00:29 -0800

    Add Api Structures
    Signed-off-by: tangyisheng2 <tangyisheng2@sina.com>

--- apis/user/user.py
+++ apis/user/user.py
@@ -3,26 +3,26 @@
 # @FileName  :user.py
 # @Time      :1/21/22
 # @Author    :Eason Tang
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, reqparse
 
 
 class LogIn(Resource):
-    @staticmethod
-    def post():
+    def post(self):
         """
         This function takes username and password and create a user
         :return: User info
         """
-        return "This function takes username and password and create a user"
+        json_data = request.get_json()  # Where it takes json data
+        return json_data
 
 
 class SignUp(Resource):
-    @staticmethod
-    def post():
+    def post(self):
         """
         This function takes username and password, verify it and login and return the userinfo
         :return:
         """
+        json_data = request.get_json()
+        return json_data
         return "This function takes username and password, verify it and login and return the userinfo"
-
-
